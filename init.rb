44c53678309977c354c3ca843097f97d15249bdc
Rupesh J <j.rupesh@gmail.com>
2014-01-06 15:17:53 +0530

    Added options onchange

--- init.rb
+++ init.rb
@@ -16,9 +16,13 @@ class UserFieldHook < Redmine::Hook::ViewListener
 		custom_field = param[:custom_field]
 		return unless custom_field.field_format == 'user'
 		f = param[:form]
-		str = f.select :role_of, [['All', '0']] + Role.all.collect{|g| [g.to_s, g.id.to_s]}, :label => l(:role_of)
-		str1 = f.select :group_of, [['None', '0']] + Group.all.collect{|g| [g.to_s, g.id.to_s]}, :label => l(:group_of)
-		"<p>#{str}</p><p>#{str1}</p>"
+		option_desc = l(:user_role_group_option)
+		str = f.select :role_of, [['All', '0']] + Role.all.collect{|g| [g.to_s, g.id.to_s]}, {:label => l(:role_of)},
+				 	:onchange => 'if (this.value != "0") {$("#custom_field_group_of").find("option:first-child").prop("selected", true).end();}'
+		str1 = f.select :group_of, [['None', '0']] + Group.all.collect{|g| [g.to_s, g.id.to_s]}, { :label => l(:group_of) },
+					:onchange => 'if (this.value != "0") {$("#custom_field_role_of").find("option:first-child").prop("selected", true).end();}'
+
+		"<p><em class='info'>#{option_desc}</em></p><p>#{str}</p><p>#{str1}</p>"
 	end
 	
 end
